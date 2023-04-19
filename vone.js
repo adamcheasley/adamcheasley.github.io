d25c8639a250589efb4274b48efe478ee427a251
Adam Forsythe-Cheasley <adamcheasley@users.noreply.github.com>
2023-04-19 12:41:30 +0100

    try setting a cookie

--- vone.js
+++ vone.js
@@ -1,1 +1,9 @@
-alert("Hello world");
+function setCookie(cname, cvalue, exdays) {
+    const d = new Date();
+    d.setTime(d.getTime() + (exdays*24*60*60*1000));
+    let expires = "expires="+ d.toUTCString();
+    document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+}
+
+alert("Hello worlds");
+setCookie("tons", "this would be the first time they hit the site", 1);
